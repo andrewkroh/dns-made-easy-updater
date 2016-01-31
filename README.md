004766c4e486b18792d89ffb30c5cbfd6a0965b4
Andrew Kroh <andrew.kroh@elastic.co>
2016-01-31 11:02:09 -0500

    Convert application to go

--- README.md
+++ README.md
@@ -1,77 +1,57 @@
 dns-made-easy-updater
 =====================
+- Download: [See releases](/andrewkroh/dns-made-easy-updater/releases)
 - Author: Andrew Kroh
 - License: Apache License, Version 2.0
-- Supported OS: any that have a JVM
-- Requirements: Java 1.5+
+- Supported OS: all supported by Go
 
 What is it?
 -----------
-The project creates two command line utilities for performing secure dynamic DNS updates 
-with DNS Made Easy. One is the manualupdater which can be used for manually performing
-an update by specifying all the parameters on the command line. The other utility is
-autoupdater which reads the configuration parameters from a properties files, stores
-the last known public IP address, and only performs an update when the IP address changes.
+This is a command line utility for updating dynamic DNS records managed by DNS
+Made Easy.
 
 How does it work?
 -----------------
-The utilities make HTTPS calls to a RESTful service hosted by DNS Made Easy. 
-See http://oldcp.dnsmadeeasy.com/enterprisedns/ddnstechspec.html for information 
-on the communication specification.
+The utility makes HTTPS calls to a RESTful service hosted by DNS Made Easy. See
+http://oldcp.dnsmadeeasy.com/enterprisedns/ddnstechspec.html for information on
+the communication specification.
 
-The utilities can be used behind NAT because they determine your public IP address using
-a "reflector" hosted by DNS Made Easy. The utilities call out to the "reflector" which
-simply echos back the IP address from which your request originated. That address is
-then used in the subsequent DDNS update.
+The utility can be used behind NAT because it determines your public IP address
+using a "reflector" hosted by DNS Made Easy. The utilities call out to the
+"reflector" which simply echos back the IP address from which the request
+originated. That address is then used in the subsequent DDNS update.
 
-Proxy Users: If you need to use this utility behind a proxy server you can set the 
-HTTP and HTTPS proxy servers using standard Java command line properties. 
-See http://download.oracle.com/javase/6/docs/technotes/guides/net/proxies.html for 
-information on how to configure your proxies. But be warned that the proxies public
-IP address will be used in your DDNS updates.
-
-How do I use it?
-----------------
-1. Build a zip or tar (see Building and Packaging below).
-2. Extract the file on the target machine.
-3. Modify config.properties to match the information for the DDNS record you want to update.
-4. Execute `./autoupdater --config-file path/to/config.properties --current-ip-file where/to/save/current-ip.properties`
-5. You should see "success" if all went well, otherwise you need to troubleshoot.
-6. Once you have the command working you'll want to automate the execution using cron or Windows Scheduled Task.
-
-For unix systems run crontab -e and add a line like this:
-	
-    * * * * * /home/user/dns-made-easy-updater/bin/autoupdater -c /home/user/dns-made-easy-updater/config.properties -i /home/user/dns-made-easy-updater/current-ip.properties >> /home/user/dns-made-easy-updater/update.log 2>&1
+Proxy Users: If you need to use this utility behind a proxy server you can set
+the  HTTP and HTTPS proxy servers using standard Go environment variables. See
+https://golang.org/pkg/net/http/#ProxyFromEnvironment But be warned that the
+proxies public IP address will be used in your DDNS updates.
 
 Command Line Usage
 ------------------
-*****
-    Usage: autoupdater [options]
-      Options:
-        -c, --config-file       Property file containing username, password, and
-                                record-id.
-                                Default: config.properties
-        -i, --current-ip-file   Property file containing the current-ip property. It
-                                will be created if it does not exist.
-                                Default: current-ip.properties
-        -d, --dont-update       No update will be performed. Used to create the
-                                initial current-ip.properties file without doing an initial
-                                update.
-                                Default: false
-*****
-    Usage: manualupdater [options]
-      Options:
-      * -p, --password    Password for updating record
-        -q, --quiet       No output during update
-                          Default: false
-      * -r, --record-id   ID number of DDNS record
-      * -u, --username    Username for updating record
+
+```
+Usage of ./dns-made-easy-updater:
+  -config string
+    	YAML config file containing username, password, record-id, and current-ip-file.
+  -current-ip-file string
+    	File containing the current public IP address. It will be created if it does not exist.
+  -insecure
+    	allow insecure SSL connections
+  -interval string
+    	Interval at which to check to IP address changes. If not given then one check (and possibly an update) is performed then it exits.
+  -password string
+    	Password for updating the DNS record.
+  -record-id string
+    	ID number of DDNS record.
+  -username string
+    	Username for updating the DNS record.
+  -v	Enable verbose output to stderr
+```
 
 Building and Packaging
 ----------------------
+You must have Go installed and have your GOPATH environment variable configured.
 
-    ./gradlew distZip distTar
+    go get github.com/andrewkroh/dns-made-easy-updater
 
-This command will build the application and package it into a zip and tar.
-The only requirement is that you have the JDK on your path. Gradle will automatically
-be downloaded by the Gradle wrapper.
+That command will download, build, and install the binary into ``$GOPATH/bin`.
---
+++ main.go
@@ -0,0 +1,261 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/joeshaw/multierror"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	ipURL     = "https://myip.dnsmadeeasy.com/"
+	updateURL = "https://cp.dnsmadeeasy.com/servlet/updateip?"
+)
+
+var (
+	interval = flag.String("interval", "", "Interval at which to check to "+
+		"IP address changes. If not given then one check (and possibly an "+
+		"update) is performed then it exits.")
+	username      = flag.String("username", "", "Username for updating the DNS record.")
+	password      = flag.String("password", "", "Password for updating the DNS record.")
+	recordID      = flag.String("record-id", "", "ID number of DDNS record.")
+	currentIPFile = flag.String("current-ip-file", "", "File containing the current "+
+		"public IP address. It will be created if it does not exist.")
+	configFile = flag.String("config", "", "YAML config file containing username, "+
+		"password, record-id, and current-ip-file.")
+	verbose  = flag.Bool("v", false, "Enable verbose output to stderr")
+	insecure = flag.Bool("insecure", false, "allow insecure SSL connections")
+)
+
+func init() {
+	flag.Parse()
+	log.SetOutput(os.Stderr)
+	log.SetLevel(log.WarnLevel)
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+}
+
+type Config struct {
+	Interval      time.Duration `yaml:"interval"`
+	Username      string        `yaml:"username"`
+	Password      string        `yaml:"password"`
+	RecordID      string        `yaml:"record-id"`
+	CurrentIPFile string        `yaml:"current-ip-file"`
+}
+
+func (c Config) validate() error {
+	var errs multierror.Errors
+	if c.Username == "" {
+		errs = append(errs, fmt.Errorf("username is required"))
+	}
+	if c.Password == "" {
+		errs = append(errs, fmt.Errorf("password is required"))
+	}
+	if c.RecordID == "" {
+		errs = append(errs, fmt.Errorf("record-id is required"))
+	}
+	if c.CurrentIPFile == "" {
+		errs = append(errs, fmt.Errorf("current-ip-file is required"))
+	}
+	return errs.Err()
+}
+
+// parseConfig interprets the command line flags are returns the config.
+func parseConfig() (Config, error) {
+	// Read YAML file.
+	c := Config{}
+	if *configFile != "" {
+		content, err := ioutil.ReadFile(*configFile)
+		if err != nil {
+			return c, err
+		}
+
+		err = yaml.Unmarshal(content, &c)
+		if err != nil {
+			return c, err
+		}
+	}
+
+	// Read CLI flags and override config file options.
+	if *interval != "" {
+		var err error
+		c.Interval, err = time.ParseDuration(*interval)
+		if err != nil {
+			return c, fmt.Errorf("invalid interval '%s'. %v", *interval, err)
+		}
+	}
+
+	// Validate the interval.
+	if c.Interval != 0 && c.Interval < 10*time.Second {
+		log.Fatalf("interval (%s) must be >= 10s", c.Interval)
+	}
+
+	if *username != "" {
+		c.Username = *username
+	}
+
+	if *password != "" {
+		c.Password = *password
+	}
+
+	if *recordID != "" {
+		c.RecordID = *recordID
+	}
+
+	if *currentIPFile != "" {
+		c.CurrentIPFile = *currentIPFile
+	}
+
+	return c, c.validate()
+}
+
+func getSavedIP(currentIPFile string) (net.IP, error) {
+	log.Printf("Loading saved IP from %s", currentIPFile)
+	content, err := ioutil.ReadFile(currentIPFile)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	return net.ParseIP(strings.TrimSpace(string(content))), nil
+}
+
+func saveIP(c Config, ip net.IP) error {
+	log.Printf("Saving IP %s to %s", ip.String(), c.CurrentIPFile)
+	return ioutil.WriteFile(c.CurrentIPFile, []byte(ip.String()+"\n"), os.FileMode(0600))
+}
+
+func getPublicIP(client *http.Client) (net.IP, error) {
+	log.Printf("Requesting public IP from %s", ipURL)
+	resp, err := client.Get(ipURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return net.ParseIP(strings.TrimSpace(string(body))), err
+}
+
+func updateDDNS(client *http.Client, c Config, ip net.IP) error {
+	log.Printf("Updating DDNS record at %s", updateURL)
+	updateQuery := fmt.Sprintf("%s?username=%s&password=%s&id=%s&ip=%s",
+		updateURL, c.Username, c.Password, c.RecordID, ip.String())
+	resp, err := client.Get(updateQuery)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	response := string(body)
+	if response == "success" {
+		return nil
+	}
+
+	return fmt.Errorf("failed to update DDNS record: %s", response)
+}
+
+func run(c Config) error {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: *insecure},
+	}
+	client := &http.Client{Transport: tr}
+
+	ip, err := getPublicIP(client)
+	if err != nil {
+		return err
+	}
+	log.Printf("Public IP %s", ip)
+
+	savedIP, err := getSavedIP(c.CurrentIPFile)
+	if err != nil {
+		return err
+	}
+	log.Printf("Saved IP %s", savedIP)
+
+	if !ip.Equal(savedIP) {
+		err = updateDDNS(client, c, ip)
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Updated record %s successfully", c.RecordID)
+
+		err = saveIP(c, ip)
+		if err != nil {
+			return fmt.Errorf("failed to save IP to disk. %v", err)
+		}
+		log.Printf("IP %s saved to %s", ip, c.CurrentIPFile)
+		return nil
+	}
+
+	log.Println("IP address is up-to-date. No changes made.")
+	return nil
+}
+
+func main() {
+	// Read config.
+	config, err := parseConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	log.Printf("Config %+v", config)
+
+	// Determine if it runs once or continuously at an interval.
+	done := make(chan struct{})
+	var tick <-chan time.Time
+	if config.Interval > 0 {
+		ticker := time.NewTicker(config.Interval)
+		tick = ticker.C
+	} else {
+		close(done)
+	}
+
+	// Shutdown gracefully when a signal is received. Wait for run to complete.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		once := sync.Once{}
+		for _ = range c {
+			once.Do(func() {
+				close(done)
+			})
+		}
+	}()
+
+	// Main loop.
+	for {
+		err := run(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		select {
+		case <-tick:
+		case <-done:
+			return
+		}
+	}
+}
